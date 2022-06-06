f72aa15333b4c2ad17a1f034fab6473f440fca4f
Tabrez-tjcg <95695149+Tabrez-tjcg@users.noreply.github.com>
2022-06-06 20:46:09 +0530

    Header Completed with 100% responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-alert("js loaded")
+// alert("js loaded")
+
+function openMenu () {
+    var nav = document.getElementById('navigations');
+    nav.classList.toggle('active');
+    // if(nav.classList.contains(active)) {
+    //     nav.style.display = "block";
+    // }
+    // else{
+    //     nav.addClass('active')
+    // }
+}
