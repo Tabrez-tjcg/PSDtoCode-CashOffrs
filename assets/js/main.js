b4a004aa5a97e0c33e191e93c988fc39b0c3a929
Tabrez-tjcg <95695149+Tabrez-tjcg@users.noreply.github.com>
2022-06-07 19:18:02 +0530

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-// alert("js loaded")
-
 function openMenu () {
     var nav = document.getElementById('navigations');
     nav.classList.toggle('active');
-    // if(nav.classList.contains(active)) {
-    //     nav.style.display = "block";
-    // }
-    // else{
-    //     nav.addClass('active')
-    // }
 }
